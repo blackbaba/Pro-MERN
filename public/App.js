860576d1399fe41359901f6e34f262f26f505051
Black Baba <amrinder.dhindsa1@gmail.com>
2019-11-05 15:32:51 +0530

    Add Babel crosscompilation setup

--- public/App.js
+++ public/App.js
@@ -1,4 +1,7 @@
+const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
+const helloContinents = Array.from(continents, c => `Hello ${c}`);
+const message = helloContinents.join(" ");
 const element = React.createElement("div", {
   title: "Outer Div"
-}, React.createElement("h1", null, "Hello World Babel"));
+}, React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById("contents"));
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
+const helloContinents = Array.from(continents, c => `Hello ${c}`);
+const message = helloContinents.join(" ");
+
 const element = (
   <div title="Outer Div">
-    <h1>Hello World Babel</h1>
+    <h1>{message}</h1>
   </div>
 );
 ReactDOM.render(element, document.getElementById("contents"));
